c41be6f956d56fc88e40edca03e5fd1942ed118d
Devika Shailesh Olkar <devikaolkar@kccemsr.edu.in>
2023-08-04 14:11:17 +0530

    added changes in the comp

---
+++ src/components/WeatherAppFun.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import "./myStyle.css"
+function WeatherAppFun() {
+  const [city, setCity] = useState("Mumbai");
+  const [weather, setWeather] = useState(null);
+  const inputRef = useRef(null);
+
+  const validateSearch = () => {
+    if (city == "") {
+      alert("Kindly enter valid email id");
+      return false;
+    }
+    return true;
+  };
+
+  const getWeather = async (city = "Mumbai") => {
+    if (validateSearch()) {
+      try {
+        const apiKey = "328442a2a4d5a6f1e9d43a77400492a3";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
+        const data = await axios.get(
+          apiUrl + `&q=${city}` + `&appid=${apiKey}`
+        );
+        console.log(data.data);
+        setWeather(data.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  let backClassName;
+  if (weather?.weather[0].main == "Clouds") {
+    backClassName = "addBackImgCloud";
+  } else if (weather?.weather[0].main == "Thunderstorm") {
+    backClassName = "addBackImgThunderStorm";
+  } else if (weather?.weather[0].main == "Rain") {
+    backClassName = "addBackImgRain";
+  } else if (weather?.weather[0].main == "Haze") {
+    backClassName = "addBackImgHaze";
+  } else if (weather?.weather[0].main == "Drizzle") {
+    backClassName = "addBackImgDrizzle";
+  } else if (weather?.weather[0].main == "Mist") {
+    backClassName = "addBackImgMist";
+  }
+
+  return (
+    <React.Fragment>
+      <div className={`weatherBody ${backClassName}`}>
+        {console.log(weather?.weather[0].main)}
+        <div className="searchBarDiv">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            ref={inputRef}
+            className="searchBarInput"
+            placeholder="Enter the City"
+          />
+          <br />
+          <button onClick={() => getWeather(city)}>
+            <img
+              className="searchImg"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkPD_ZmqvYRoN22NGAiTG-EuD4I9FACxmRCA&usqp=CAU"
+            />
+          </button>
+        </div>
+        <br />
+        <div className="tempCityDiv">
+          <h3>{weather?.main?.temp}°C</h3>
+          <h4>{city}</h4>
+        </div>
+        <div className="humSpeedDiv">
+          <div>
+            <img
+              className="humIcon"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNOyMq1pQBdiNManiDBmhG6wdEPGftn44gZmAOy7LqJzP-w0WQ2k_n-MEro-BNudWLjP0&usqp=CAU"
+            />
+            <div className="weatherHum">
+              <h4>{weather?.main?.humidity}%</h4>
+              <h5>Humidity</h5>
+            </div>
+          </div>
+
+          <div>
+            <img
+              className="humIcon"
+              src="https://img.freepik.com/premium-vector/wind-clouds-icon-weather-forecast-pictogram-wind-icon-wind-blowing-windy-weather-air-icons-doodle-wind-winds-clouds-weather-symbol-wind-speed-icon_754208-73.jpg"
+            />
+            <div className="weatherHum">
+              <h4>{weather?.wind?.speed} km/hr</h4>
+              <h5>Wind Speed</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default WeatherAppFun;
